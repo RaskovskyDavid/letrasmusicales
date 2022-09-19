412a8c9a92706cc7c79839b638e56a998ce0333b
david <davidtucu@hotmail.com>
2022-09-19 12:18:57 -0300

    implementando axios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-
+import { useState } from 'react'
+import useLetras from '../hooks/useLetras'
 const Formulario = () => {
+  const { setAlerta, busquedaLetra} = useLetras()
+  const [busqueda, setBusqueda] = useState({
+    artista: '',
+    cancion: ''
+})
+const handleSubmit = e => {
+  e.preventDefault()
+
+  if(Object.values(busqueda).includes('')) {
+      setAlerta('Coloca nombre de artista y canción')
+      return
+  }
+  busquedaLetra(busqueda)
+  setAlerta("")
+
+  
+}
   return (
-    <div>
-      Formulario
-    </div>
+    <form
+    onSubmit={handleSubmit}
+    >
+      <legend> Buscar por Artistas y Canciones</legend>
+      <div className="form-grid">
+                <div>
+                    <label>Artista</label>
+                    <input
+                        type="text"
+                        name="artista"
+                        placeholder="Nombre Artista"
+                        value={busqueda.artista}
+                        onChange={ e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name] : e.target.value
+                        })}
+                    />
+                </div>
+                <div>
+                    <label>Canción</label>
+                    <input
+                        type="text"
+                        name="cancion"
+                        placeholder="Nombre Canción"
+                        value={busqueda.cancion}
+                        onChange={ e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name] : e.target.value
+                        })}
+                      
+                    />
+                </div>
+
+                <input type="submit" value="Buscar" />
+      </div>
+    </form>
   )
 }
 
